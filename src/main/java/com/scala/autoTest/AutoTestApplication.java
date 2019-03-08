991e94ab34741ea352aa59944aa82148306dd8db
scala804 <scala804@163.com>
2019-03-08 10:32:35 +0800

    提交测试

--- src/main/java/com/scala/autoTest/AutoTestApplication.java
+++ src/main/java/com/scala/autoTest/AutoTestApplication.java
@@ -2,6 +2,7 @@ package com.scala.autoTest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class AutoTestApplication {
@@ -9,5 +10,4 @@ public class AutoTestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AutoTestApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/scala/autoTest/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.scala.autoTest.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class TestController {
+
+    @RequestMapping("/hello")
+    private String index(){
+        return "Hello World!";
+    }
+}
