76c43aab1a3fe86e0c9328e92d81755721c8773d
scala804 <scala804@163.com>
2019-03-12 10:09:42 +0800

    log4j

--- src/main/java/com/scala/autoTest/restClient/RestClient.java
+++ src/main/java/com/scala/autoTest/restClient/RestClient.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+
+import org.apache.log4j.Logger;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -14,14 +16,18 @@ import org.apache.http.impl.client.HttpClients;
 
 
 
+
 public class RestClient {
 
+
+    final static Logger Log = Logger.getLogger(RestClient.class);
     //1. Get 请求方法
     public CloseableHttpResponse get(String url) throws ClientProtocolException, IOException {
     //创建一个可关闭的HttpClient对象
         CloseableHttpClient httpclient = HttpClients.createDefault();
      //创建一个HttpGet的请求对象
         HttpGet httpget = new HttpGet(url);
+        Log.info("httpget="+httpget);
      //执行请求,相当于postman上点击发送按钮，然后赋值给HttpResponse对象接收
         CloseableHttpResponse httpResponse = httpclient.execute(httpget);
 
@@ -34,6 +40,7 @@ public class RestClient {
         CloseableHttpClient httpclient = HttpClients.createDefault();
 //创建一个HttpGet的请求对象
         HttpGet httpget = new HttpGet(url);
+
 //加载请求头到httpget对象
         for (Map.Entry<String, String> entry : headermap.entrySet()) {
             httpget.addHeader(entry.getKey(), entry.getValue());
@@ -51,6 +58,7 @@ public class RestClient {
         CloseableHttpClient httpclient = HttpClients.createDefault();
        //创建一个HttpPost的请求对象
         HttpPost httppost = new HttpPost(url);
+
        //设置payload
         httppost.setEntity(new StringEntity(entityString));
 
