060371191c9d023facbbd8f3f3b93b37e285ea89
scala804 <scala804@163.com>
2019-03-18 08:43:26 +0800

    报告生成

--- src/main/java/com/scala/autoTest/base/ExtentTestNGIReporterListener.java
+++ src/main/java/com/scala/autoTest/base/ExtentTestNGIReporterListener.java
@@ -1,5 +1,6 @@
 package com.scala.autoTest.base;
 
+/*import com.aventstack.extentreports.ExtentReports;*/
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.ResourceCDN;
