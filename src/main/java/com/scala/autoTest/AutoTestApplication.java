9d191f02b723d5684ab8d3df9f67d855371e2088
scala804 <scala804@163.com>
2019-04-09 17:17:23 +0800

    增加EnableScheduling

--- src/main/java/com/scala/autoTest/AutoTestApplication.java
+++ src/main/java/com/scala/autoTest/AutoTestApplication.java
@@ -2,9 +2,11 @@ package com.scala.autoTest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-@SpringBootApplication
+@EnableScheduling
+@SpringBootApplication(exclude = MongoAutoConfiguration.class)
 public class AutoTestApplication {
 
 	public static void main(String[] args) {
