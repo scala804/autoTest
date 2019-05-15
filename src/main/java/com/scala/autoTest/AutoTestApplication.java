d6f994cd5ffcd0b01c651c6d7e1a0ba4ba3d93d1
scala804 <scala804@163.com>
2019-05-15 14:27:22 +0800

    11

--- src/main/java/com/scala/autoTest/AutoTestApplication.java
+++ src/main/java/com/scala/autoTest/AutoTestApplication.java
@@ -12,29 +12,5 @@ public class AutoTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AutoTestApplication.class, args);
-		/*TestNG testNG = new TestNG();
-		testNG.setTestClasses(new Class[] {PostApiTest.class});
-		testNG.run();*/
-
-		/*XmlSuite suite = new XmlSuite();
-		suite.setName("TmpSuite");
-		XmlTest test = new XmlTest(suite);
-		test.setName("TmpTest");
-		List<XmlClass> classes = new ArrayList<XmlClass>();
-		classes.add(new XmlClass("com.scala.autoTest.test"));
-		test.setXmlClasses(classes) ;
-
-
-		List<XmlSuite> suites = new ArrayList<XmlSuite>();
-		suites.add(suite);
-		TestNG tng = new TestNG();
-		tng.setXmlSuites(suites);
-		tng.run();*/
-/*
-		TestNG testNG = new TestNG();
-		List<String> suites = new ArrayList<String>();
-		suites.add("res\\testNG.xml");//此处为xml的绝对路径
-		testNG.setTestSuites(suites);
-		testNG.run();*/
 	}
 }
--- src/main/java/com/scala/autoTest/autoTestCase/testDataConfig.java
+++ src/main/java/com/scala/autoTest/autoTestCase/testDataConfig.java
@@ -4,12 +4,8 @@ package com.scala.autoTest.autoTestCase;
 import com.scala.autoTest.AutoTestApplication;
 import com.scala.autoTest.bean.sys.SysConfig;
 import com.scala.autoTest.dao.sys.SysConfigMapper;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.stereotype.Component;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
 
